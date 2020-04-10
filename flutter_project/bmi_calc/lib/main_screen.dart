c0d66f69888e3d906de506aa8467880da75b08d4
unknown <lamilekan263@gmail.com>
2020-04-09 23:55:05 -0700

    working on my bmi calculator

--- flutter_project/bmi_calc/lib/main_screen.dart
+++ flutter_project/bmi_calc/lib/main_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class BmiScreen extends StatefulWidget {
   @override
   _BmiScreenState createState() => _BmiScreenState();
@@ -10,9 +9,48 @@ class _BmiScreenState extends State<BmiScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        
+        appBar: AppBar(
+          title: Text('BMI Calculator'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(child: BmiChild()),
+                  Expanded(child: BmiChild()),
+                ],
+              ),
+            ),
+            Expanded(child: BmiChild()),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(child: BmiChild()),
+                  Expanded(child: BmiChild()),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class BmiChild extends StatelessWidget {
+  final colour;
+
+  BmiChild({@required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
+      margin: EdgeInsets.all(10.0),
     );
   }
 }
