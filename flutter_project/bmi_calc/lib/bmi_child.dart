8441f1222b375dd75aafee80000176a88fd41239
unknown <lamilekan263@gmail.com>
2020-04-10 00:59:39 -0700

    Structured my files properly

---
+++ flutter_project/bmi_calc/lib/bmi_child.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BmiChild extends StatelessWidget {
+  final Color color;
+  final Widget cardChild;
+
+  BmiChild({this.color, this.cardChild});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: cardChild,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      margin: EdgeInsets.all(10.0),
+    );
+  }
+}
--- flutter_project/bmi_calc/lib/main_screen.dart
+++ flutter_project/bmi_calc/lib/main_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'bmi_child.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BmiScreen extends StatefulWidget {
   @override
@@ -10,51 +11,42 @@ class _BmiScreenState extends State<BmiScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('BMI Calculator'),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(child: BmiChild()),
-                  Expanded(child: BmiChild()),
-                ],
-              ),
-            ),
-            Expanded(child: BmiChild()),
-            Expanded(
-              child: Row(
-                children: <Widget>[
-                  Expanded(child: BmiChild(
-                    cardChild: ,
-                  )),
-                  Expanded(child: BmiChild()),
-                ],
-              ),
-            ),
-          ],
-        ));
-  }
-}
-
-class BmiChild extends StatelessWidget {
-  final Color color;
-  final Widget cardChild;
-
-  BmiChild({this.color, this.cardChild});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+        centerTitle: true,
       ),
-      margin: EdgeInsets.all(10.0),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: BmiChild(),
+                ),
+                Expanded(
+                  child: BmiChild(),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: BmiChild(),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: BmiChild(),
+                ),
+                Expanded(
+                  child: BmiChild(),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
+
